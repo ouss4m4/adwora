0fedd5267e38299882ae52ea62e49afa6a1a5712
Oussama <oussama@notifyai.io>
2024-12-02 11:01:05 +0400

    add templates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,17 @@ const router = Router();
 
 // Home route
 router.get("/", (req, res) => {
-  res.render("dashboard", { title: "Home Page" });
+  res.render("dashboard", { title: "Dashboard | Adwora" });
+});
+
+// Client Route
+router.get("/clients", (req, res) => {
+  res.render("clients", { title: "Clients Page" });
+});
+
+// User Route
+router.get("/users", (req, res) => {
+  res.render("users", { title: "Users Page" });
 });
 
 export default router;
